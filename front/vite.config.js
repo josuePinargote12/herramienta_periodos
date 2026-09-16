d06bab3aa2b2ca860859fcdf228c6202348f2377
Santiago <santy.moreira88@gmail.com>
2026-09-16 18:04:34 -0500

    Remove specific allowed hosts and unnecessary proxy configuration in Vite setup

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,12 +6,7 @@ export default defineConfig({
   cacheDir: '../.vite-cache-front',
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['turret-dynasty-twine.ngrok-free.dev'],
     proxy: {
-      '/sso-test': {
-        target: 'http://127.0.0.1:80',
-        changeOrigin: true
-      },
       '/api': {
         target: 'http://127.0.0.1:3000',
         changeOrigin: true
